cfc5193cd2c64188fe9657ab743d0f9d8bdb6f87
Ramindu Walgama <raminduw200@gmail.com>
2023-05-31 16:56:04 +0530

    Add scroll animations

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './home.css'
 import video from '../../Assets/132140.mp4'
-import { GrLocation, } from 'react-icons/gr'
+
+import { GrLocation } from 'react-icons/gr'
 import { HiFilter } from 'react-icons/hi'
 import { FiFacebook } from 'react-icons/fi'
 import { AiOutlineInstagram } from 'react-icons/ai'
@@ -9,7 +10,14 @@ import { SiTripadvisor } from 'react-icons/si'
 import { BsListTask } from 'react-icons/bs'
 import { TbApps } from 'react-icons/tb'
 
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 const Home = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000 })
+    }, [])
+
     return (
         <section className='home'>
             <div className="overlay"></div>
@@ -18,17 +26,17 @@ const Home = () => {
             <div className="homeContent container">
                 <div className="textDiv">
 
-                    <span className="smallText">
+                    <span data-aos="fade-up" className="smallText">
                         Our Packages
                     </span>
 
-                    <h1 className="homeTitle">
+                    <h1 data-aos="fade-up" className="homeTitle">
                         Search your Holiday
                     </h1>
 
                 </div>
 
-                <div className="cardDiv grid">
+                <div data-aos="fade-up" className="cardDiv grid">
                     <div className="destinationInput">
                         <label htmlFor="city">Search your destination:</label>
                         <div className="input flex">
@@ -57,7 +65,7 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div className="homeFooterIcons flex">
+                <div  data-aos="fade-up" className="homeFooterIcons flex">
                     <div className="rightIcons">
                         <FiFacebook className='icon'/>
                         <AiOutlineInstagram className='icon'/>
