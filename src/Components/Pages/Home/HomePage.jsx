58a1dddbdee8d9d90bb74c076e5c88272e3b0ad3
Ramindu Walgama <raminduw200@gmail.com>
2023-06-11 12:00:08 +0530

    Restructure folder structure/pages
    Render pages separately in each page folder and call it from the app

---
+++ src/Components/Pages/Home/HomePage.jsx
@@ -0,0 +1,15 @@
+import Home from './Home/Home';
+import Main from './Main/Main';
+import Feedback from './Feedback/Feedback';
+
+function HomePage() {
+  return (
+    <>
+    <Home />
+    <Main />
+    <Feedback />
+    </>
+  );
+}
+
+export default HomePage;
---
+++ src/Components/Pages/PackageDetail/PackageDetailPage.jsx
@@ -0,0 +1,13 @@
+import Header from './Header/Header';
+import Body from './Body/Body';
+
+function HomePage() {
+  return (
+    <>
+    <Header />
+    <Body />
+    </>
+  );
+}
+
+export default HomePage;
