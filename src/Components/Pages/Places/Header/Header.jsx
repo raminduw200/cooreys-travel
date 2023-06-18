c0d652a90ea6697661f319b9fc3cd1325176b65e
Ramindu Walgama <raminduw200@gmail.com>
2023-06-18 16:07:49 +0530

    Remove unused router dom in header

--- src/Components/Pages/Places/Header/Header.jsx
+++ src/Components/Pages/Places/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import './header.css'
-import { useLocation } from 'react-router-dom';
 
 import { FiFacebook } from 'react-icons/fi'
 import { AiOutlineInstagram } from 'react-icons/ai'
