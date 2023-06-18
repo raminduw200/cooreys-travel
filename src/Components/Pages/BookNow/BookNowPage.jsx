55dd6a5ae79900cc29063162bd0b08f31d63df95
Ramindu Walgama <ramindu.walgama@ntender.ai>
2023-06-18 15:58:55 +0530

    Connect BookNow page components

---
+++ src/Components/Pages/BookNow/BookNowPage.jsx
@@ -0,0 +1,13 @@
+import Body from './Body/Body';
+import Header from './Header/Header';
+
+function BookNowPage() {
+    return (
+        <>
+        <Header />
+        <Body />
+        </>
+    );
+}
+
+export default BookNowPage;
