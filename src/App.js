d72e00cd101a72ef98d00759e6e4ce4766b8322b
Ramindu Walgama <ramindu.walgama@ntender.ai>
2023-06-18 15:51:52 +0530

    Add Booking page and Places page routings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import HomePage from './Components/Pages/Home/HomePage';
 import PackageDetailPage from './Components/Pages/PackageDetail/PackageDetailPage';
+import BookNowPage from './Components/Pages/BookNow/BookNowPage';
+import PlacesListPage from './Components/Pages/Places/PlacesList';
+
 import NavBar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer'
 
@@ -13,6 +17,8 @@ function App() {
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="/package_detail" element={<PackageDetailPage />} />
+        <Route path="/book_now" element={<BookNowPage />} />
+        <Route path="/places" element={<PlacesListPage />} />
         <Route path="*" element={<HomePage />} />
       </Routes>
       <Footer />
