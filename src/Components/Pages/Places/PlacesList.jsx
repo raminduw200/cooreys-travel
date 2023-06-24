e0c711a392469b32adf0b9ddf7c871d39abcd11b
Ramindu Walgama <raminduw200@gmail.com>
2023-06-24 10:19:01 +0530

    Add places page

---
+++ src/Components/Pages/Places/PlacesList.jsx
@@ -0,0 +1,15 @@
+import Body from './Body/Body';
+import Header from './Header/Header';
+import Card from './Cards/Card';
+
+function PlacesListPage() {
+    return (
+        <>
+        <Header />
+        <Body />
+        <Card />
+        </>
+    );
+}
+
+export default PlacesListPage;
