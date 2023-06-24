2c6ef7cac4369f7d3cc12ffb24b003e2fc56de7b
Ramindu Walgama <raminduw200@gmail.com>
2023-06-25 01:22:04 +0530

    Add place routing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomePage from './Components/Pages/Home/HomePage';
 import PackageDetailPage from './Components/Pages/PackageDetail/PackageDetailPage';
 import BookNowPage from './Components/Pages/BookNow/BookNowPage';
 import PlacesListPage from './Components/Pages/Places/PlacesList';
+import PlaceDetailPage from './Components/Pages/PlaceDetail/PlaceDetailPage';
 
 import NavBar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer'
@@ -19,6 +20,7 @@ function App() {
         <Route path="/package_detail" element={<PackageDetailPage />} />
         <Route path="/book_now" element={<BookNowPage />} />
         <Route path="/places" element={<PlacesListPage />} />
+        <Route path="/place" element={<PlaceDetailPage />} />
         <Route path="*" element={<HomePage />} />
       </Routes>
       <Footer />
